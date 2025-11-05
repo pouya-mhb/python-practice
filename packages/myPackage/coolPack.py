ae5f9249ea4c481a58fec5662908b06a302476d4
Pouya <pouya.mehrbabak@gmail.com>
2025-11-05 14:28:01 +0330

    Add files via upload

---
+++ packages/myPackage/coolPack.py
@@ -0,0 +1,7 @@
+
+def cool_pack(name):
+    return f"You are so cool {name}"
+
+
+if __name__ == "__main__" :
+    print ("you should use me as a package  !")
---
+++ packages/myProg.py
@@ -0,0 +1,5 @@
+from myPackage import coolPack
+
+name = input("Enter your name : ")
+
+print(coolPack.cool_pack(name))
